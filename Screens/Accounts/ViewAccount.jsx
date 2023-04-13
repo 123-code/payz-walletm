c976d752b6558ecb30d4cf29a46f8756743ea4dc
123-code <naranjojose256@gmail.com>
2023-04-12 23:56:03 -0500

    _-_

--- Screens/Accounts/ViewAccount.jsx
+++ Screens/Accounts/ViewAccount.jsx
@@ -1,21 +1,34 @@
 import React,{useState,useEffect} from 'react';
 import { View, Text, StyleSheet,Pressable } from 'react-native';
 import { ListItem,Avatar,Button } from '@rneui/themed';
+import {GetInsertedData} from '../../util/SQLiteconn';
 import {GetWalletBTCBalance} from '../../util/GetWalletBTCBalance';
 import WalletBalance from '../../Components/WalletBalance';
 
 
 
 export default function ViewAccount() {
-  const[USDBalance,setUSDBalance] = useState(null)
-  const[BTCBalance,setBTCBalance] = useState(null)
-  const[SATBalance,setSATBalance] = useState(null)
+  const[USDBalance,setUSDBalance] = useState(0)
+  const[BTCBalance,setBTCBalance] = useState(0)
+  const[SATBalance,setSATBalance] = useState(0)
+  const [Account,SetAccount] = useState([]);
 
   const BTCImageURL = "https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/Bitcoin.svg/300px-Bitcoin.svg.png"
   const USDImageURL = "https://img.freepik.com/premium-vector/dollar-icon-american-currency-symbol-banknote_572070-170.jpg?w=1060"
 
+const AccountGetter = async()=>{
+  const account =  await GetInsertedData();
+  /* --TODO in the case of specific selection
+  create sqlite query that triggers public key by ID
+  */
+  const MYAccount = account[0]
+  SetAccount(MYAccount)
+  console.log("dwidiwrf",Account)
+}
+
   useEffect(()=>{
-    async function GetWalletValues(account){
+    AccountGetter()
+      async function GetWalletValues(account){
       const testbalanceURL = "https://blockstream.info/api/address/"
       const walletaddress = await fetch(`${testbalanceURL}${account}`)
       const walletinfo = await walletaddress.json()
@@ -29,7 +42,7 @@ export default function ViewAccount() {
       setBTCBalance(BTCBalance)
       setSATBalance(getwalletBalance)
   }
-  GetWalletValues("1BvBMSEYstWetqTFn5Au4m4GFg7xJaNVN2")
+  GetWalletValues(Account)
     //WalletBalance("1BvBMSEYstWetqTFn5Au4m4GFg7xJaNVN2")
   },[])
     return(

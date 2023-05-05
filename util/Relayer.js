fb8143cc4df18359aa16a28e6c9cc121261183b5
123-code <naranjojose256@gmail.com>
2023-05-05 00:17:06 -0500

    Relayer & contracts initialized

---
+++ util/Relayer.js
@@ -0,0 +1,12 @@
+import { RpcRelayer } from '@0xsequence/relayer'
+import { Wallet } from '@0xsequence/wallet'
+import { ethers } from 'ethers'
+import dotenv from 'dotenv';
+dotenv.config();
+
+const provider = new ethers.providers.JsonRpcProvider('https://nodes.sequence.app/polygon')
+const walletEOA = new ethers.Wallet(process.env.SERVER_EOA_PRIVATE_KEY, provider)
+
+//relayer RPC
+const relayer = new RpcRelayer({url: 'https://polygon-relayer.sequence.app', provider: provider})
+const wallet = (await Wallet.singleOwner(walletEOA)).connect(provider, relayer)

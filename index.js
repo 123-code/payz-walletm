6b386eda2850bbe634e10da2757b1105b81a0029
123-code <naranjojose256@gmail.com>
2023-07-06 11:03:29 -0500

    app errors

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
-import { registerRootComponent } from 'expo';
-
+import { AppRegistry } from 'react-native';
 import App from './App';
 
-// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
-// It also ensures that whether you load the app in Expo Go or in a native build,
-// the environment is set up appropriately
-registerRootComponent(App);
+// Register the "main" component using AppRegistry
+AppRegistry.registerComponent('main', () => App);
